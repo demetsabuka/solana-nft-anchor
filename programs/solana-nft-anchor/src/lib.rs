daa92c818ce49194396777c6817ad9d2ea6d8f50
demetsabuka <156785447+demetsabuka@users.noreply.github.com>
2024-01-16 17:16:17 +0700

    Update lib.rs

--- programs/solana-nft-anchor/src/lib.rs
+++ programs/solana-nft-anchor/src/lib.rs
@@ -12,7 +12,7 @@ use mpl_token_metadata::{
     state::DataV2,
 };
 
-declare_id!("<UPDATE HERE>");
+declare_id!("5PYH45BKMoyNgaKQDq95TnP3Rp6QcX11NkVdmMWTbmsM");
 #[program]
 pub mod solana_nft_anchor {
 
